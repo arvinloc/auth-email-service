025181ca4fdafe63b26a89c7b35d1f8a2f22d9e9
arvinloc <sulvinskijartiom@gmail.com>
2026-09-17 10:15:17 +0300

    feat: username deleted from orm model

--- app/db/models/user.py
+++ app/db/models/user.py
@@ -1,9 +1,9 @@
 from app.core.database import Base
-from sqlalchemy import Column,Integer,String
+from sqlalchemy import Column,Integer,String,Boolean
 
 class User(Base):
     __tablename__ = "Users"
     id = Column(Integer,primary_key = True)
-    username = Column(String(50))
     email = Column(String(70), unique=True)
     password = Column(String(250))
+    is_verified = Column(Boolean,default=False)
