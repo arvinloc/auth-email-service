d68498b6d1c6f973d240809e741c879aef759c69
arvinloc <sulvinskijartiom@gmail.com>
2026-09-17 10:17:54 +0300

    refactor: orm magicLink initialization

--- app/util/init_db.py
+++ app/util/init_db.py
@@ -1,6 +1,6 @@
 from app.core.database import Base,engine
 from app.db.models.user import User
-
+from app.db.models.magicLinkToken import MagicLinkToken
 
 def create_tables():
     Base.metadata.create_all(bind=engine)
